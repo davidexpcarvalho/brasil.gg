00b43f134ed39a1b788b3d3776f03ee31e057fc8
David Expósito de Carvalho <69867572+davidexpcarvalho@users.noreply.github.com>
2024-06-04 09:12:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from flask import Flask, request, jsonify
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from brasilgg import Player, DATABASE_URL
 
 app = Flask(__name__)
 
+# Configuração do banco de dados
+DATABASE_URL = 'sqlite:///players.db'
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
+
+class Player(Base):
+    __tablename__ = 'players'
+    id = Column(Integer, primary_key=True)
+    player_name = Column(String)
+    nick = Column(String)
+    tag_line = Column(String)
+    team_name = Column(String)
+    puuid = Column(String)
+
+Base.metadata.create_all(engine)
 
 @app.route('/players', methods=['POST'])
 def add_player():
