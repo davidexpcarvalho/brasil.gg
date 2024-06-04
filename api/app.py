62111d9f62090cbd637e66a38d518e3ee0949809
David Expósito de Carvalho <69867572+davidexpcarvalho@users.noreply.github.com>
2024-06-04 10:35:02 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,19 +1,35 @@
-from flask import Flask, jsonify, request
-from brasilgg import Player, session, get_puuid, save_progress_to_db, get_existing_match_ids, get_champion_translation
+# api/app.py
+from flask import Flask, render_template, request, redirect, url_for
+from brasilgg import Player, session, get_puuid
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({"message": "API está funcionando!"})
-
-@app.route("/players", methods=["GET"])
-def get_players():
     players = session.query(Player).all()
-    player_data = [{"player_name": player.player_name, "nick": player.nick, "team_name": player.team_name} for player in players]
-    return jsonify(player_data)
+    return render_template("index.html", players=players)
 
-# Adicione outros endpoints conforme necessário
+@app.route("/add_player", methods=["GET", "POST"])
+def add_player():
+    if request.method == "POST":
+        player_name = request.form["player_name"]
+        nick = request.form["nick"]
+        tag_line = request.form["tag_line"]
+        team_name = request.form["team_name"]
+        api_key = "YOUR_API_KEY"  # Substitua pelo valor da sua chave de API
+
+        # Obtém o PUUID do jogador
+        puuid = get_puuid(nick, tag_line, api_key)
+
+        if puuid:
+            new_player = Player(player_name=player_name, nick=nick, tag_line=tag_line, team_name=team_name, puuid=puuid)
+            session.add(new_player)
+            session.commit()
+            return redirect(url_for("index"))
+        else:
+            return "Erro ao obter o PUUID. Verifique os detalhes do jogador e tente novamente."
+
+    return render_template("add_player.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
