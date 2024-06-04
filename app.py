14f2ae377da007caa6320369a714c0953d2fcfb1
David Expósito de Carvalho <69867572+davidexpcarvalho@users.noreply.github.com>
2024-06-04 10:44:08 -0300

    Alteração geral

--- app.py
+++
@@ -1,9 +0,0 @@
-from api.app import app
-
-# Verificação simples para ver se o servidor está funcionando
-@app.route("/")
-def index():
-    return "API está funcionando!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
