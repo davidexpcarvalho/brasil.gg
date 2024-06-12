eca6604631bdbb632cacd3780982c93b0a86d557
David Expósito de Carvalho <69867572+davidexpcarvalho@users.noreply.github.com>
2024-06-12 11:16:07 -0300

    Update analise.py

--- analise.py
+++ analise.py
@@ -6,6 +6,12 @@ import mysql.connector
 from mysql.connector import Error
 import json
 
+print("DB_USER:", os.getenv("DB_USER"))
+print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
+print("DB_HOST:", os.getenv("DB_HOST"))
+print("DB_PORT:", os.getenv("DB_PORT"))
+print("DB_NAME:", os.getenv("DB_NAME"))
+
 def connect_to_db(retries=5, delay=5):
     for attempt in range(retries):
         try:
